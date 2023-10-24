cbf8da113ce0d004cbd333907aaf8592a028d71d
Keegan <keegancolaco20@gmail.com>
2023-10-24 17:59:20 +0530

    Desktop UI

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -10,8 +10,8 @@ const RestaurantCard = ({
 return (
         <div className="card w-[235px] h-[330px] bg-amber-100 rounded-lg  m-3 shadow-lg">
         < img src={IMG_CDN_URL + cloudinaryImageId} className='w-full h-[180px] rounded-t-xl' />
-           <div className="card-content">
-            <h2 className="RestaurantName">{name}</h2>
+           <div className="card-content px-2">
+            <h2 className="RestaurantName font-medium text-center">{name}</h2>
             <div className="sub-content">
             <p>{locality } </p>
             <p className="cuisines">{cuisines.slice(0, 4).join(", ")}</p>
