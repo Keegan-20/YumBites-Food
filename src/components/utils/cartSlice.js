a820cf846bd58101e3d3ecc5273a8d032a755dd8
Keegan <keegancolaco20@gmail.com>
2024-01-19 17:24:05 +0530

    creating Slice in the redux store

--- src/components/utils/cartSlice.js
+++ src/components/utils/cartSlice.js
@@ -1,14 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//creating a slice in the Redux store
-const  cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-    items:[],
-   },
-   reducers:{
-    addItem: (state,action) => {
-       state.items.push(action.payload);
-    }
-   }  
-})
+//creating  slices in the Redux store
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: ["goa", "rajasthan", "benagluru"], //initial state values
+  },
+  reducers: {
+      //addItem :action Name where state & actions  are  arguments
+      addItem: (state, action) => {
+      state.items.push(action.payload); //action.payload is used to send data
+    },
+      removeItem: (state, action) => {
+      state.items.pop();
+    },
+      clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+//addItem, removeItem, clearCart are all actions  that will call a reducer function
+export const {addItem,removeItem,clearCart} = cartSlice.actions;
+export default cartSlice.reducer;
--- src/components/utils/store.js
+++ src/components/utils/store.js
@@ -1,9 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit"
+import cartSlice from "./cartSlice";
 
 //we have used reduxtoolkit to create redux store
 const store =configureStore({
-
-
+reducer:{
+    cart:cartSlice,
+ },
 });
 
 export default store;
+
+/** using reduxtoolkit:
+ * 1. Creating redux store:
+ * -configureStore()---RTK
+ * 
+ * 2.Provide my store to app:
+ * --Provider Component = {store} --imported from react-redux
+ * 
+ * 3.Creating a Slice---RTK
+ * -createSlice({
+ * name:"",
+ * initialState:
+ * reducers:{
+ * actionName:(state,action)=>{ reducer func}
+ * }
+ * })
+ * 
+ * 4.exporting the cartSlice
+ * - export const {addItem,removeItem,clearCart}=cartSlice.actions
+ * - export default cartSlice.reducer;
+ * 
+ * 5.Putting the slice into the store
+ * -{
+ * reducer:{
+ * cart:cartSlice,
+ * user:userSlice
+ * }}
+ * 
+ */
