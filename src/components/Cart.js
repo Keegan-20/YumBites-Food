7af1a04212f4bf2662b0737892bc1322de88e910
Keegan <keegancolaco20@gmail.com>
2024-01-23 13:27:47 +0530

    Handling State Management using redux-toolkit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,24 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import MenuCart from "./MenuCart";
+import { clearCart } from "./utils/cartSlice";
 
 const Cart = () =>{
     const cartItems=useSelector((store) => store.cart.items);
 
+    dispatch= useDispatch();
+     const handClearCart = () => {
+        dispatch(clearCart()); 
+     }   
+
     return (
-    <div className="flex flex-wrap">
+    <div >
         <h1 className="font-bold text-2xl text-center">Cart Items :{cartItems.length}</h1>
+        <button className="bg-red-400 text-white ml-2 p-2 " onClick={()=>handClearCart()}>Clear Cart</button>
+        <div className="flex flex-wrap">
         {cartItems.map((item)=>(
             <MenuCart key={item.id} {...item}  />
         ))}
+        </div>
     </div>
     )
 }
--- src/components/MenuCart.js
+++ src/components/MenuCart.js
@@ -3,12 +3,13 @@ import { IMG_CDN_URL } from '../constant';
 import 'react-loading-skeleton/dist/skeleton.css'
 
 const MenuCart = ({
-  name,description,
+  name,
   imageId,price,itemAttribute
   //   sla // This is the entire 'sla' object
 }) => {
  // Accessing the 'vegClassifier' property from 'itemAttribute'
  const vegClassifierValue = itemAttribute && itemAttribute.vegClassifier;
+
 //  The && (logical AND) operator is used to check if itemAttribute exists (is truthy) before attempting to access the vegClassifier property.
   //   const { deliveryTime, lastMileTravelString } = sla; // Accessing nested loo
   return (
@@ -16,7 +17,7 @@ const MenuCart = ({
       < img src={IMG_CDN_URL + imageId} className='w-full h-[180px] rounded-t-xl ' />
       <div className="card-content px-2">
         <h2 className="RestaurantName font-medium text-center">{name}</h2>
-        <h4 className='font-semibold'> <i className="fa-solid fa-star bg-green-600 text-xs p-1  mb-1 mr-1 text-[#fff] rounded-[50%]"></i> {description}</h4>
+        <h4 className='font-semibold'> <i className="fa-solid fa-star bg-green-600 text-xs p-1  mb-1 mr-1 text-[#fff] rounded-[50%]"></i></h4>
         <h4>{vegClassifierValue}</h4>
        <h4>Rupees: {price/100}</h4>
       </div>
