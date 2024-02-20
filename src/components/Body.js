d98001341f0b9db595b90c818062298e5a011b99
Keegan <keegancolaco20@gmail.com>
2024-02-21 05:09:27 +0530

    feat :  added a Carousel  Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,78 +1,90 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
-import 'react-loading-skeleton/dist/skeleton.css';
+import "react-loading-skeleton/dist/skeleton.css";
 import { Link } from "react-router-dom";
 import { filterData } from "./utils/helper";
+import Carousel from "./Carousel";
 import useOnline from "../Custom Hooks/useOnline";
 import { swiggy_restaurant_details } from "../constant";
 
 const Body = () => {
   //let searchText="OLA" //normal js
   const [searchText, setSearchText] = useState(""); //returns =[variable name,function to update the variable]
+  const [carouselCards, setCarouselCards] = useState([]);
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const [loading, setLoading] = useState(true); 
-  const[errorMessage,setErrorMessage] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState([]);
 
-  useEffect(() => { //callback fn will be called once after the render()
+  useEffect(() => {
+    //callback fn will be called once after the render()
     //Api call
     getRestaurants(); //sideffect:api calling
   }, []);
-
   async function getRestaurants() {
-    const data = await fetch(
-      swiggy_restaurant_details
-    );
-    const json = await data.json();
+  try {
+      const data = await fetch(swiggy_restaurant_details);
+      const json = await data.json();
 
-    // initialize checkJsonData() function to check Swiggy Restaurant data
-    async function checkJsonData(jsonData) {
-      for (let i = 0; i < jsonData?.data?.cards.length; i++) {
-
-        // initialize checkData for Swiggy Restaurant data i=5
-        let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        console.log(checkData);
-        // if checkData is not undefined then return it
-        if (checkData !== undefined) {
-          return checkData;
+      // initialize checkJsonData() function to check Swiggy Restaurant data
+      async function checkJsonData(jsonData) {
+        for (let i = 0; i < jsonData?.data?.cards.length; i++) {
+          // initialize checkData for Swiggy Restaurant data i=5
+          let checkData =
+            json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle
+              ?.restaurants;
+          console.log(checkData);
+          // if checkData is not undefined then return it
+          if (checkData !== undefined) {
+            return checkData;
+          }
         }
       }
-    }
 
-    // call the checkJsonData() function which return Swiggy Restaurant data
-    const resData = await checkJsonData(json);
-    setAllRestaurants(resData); //initially show all restaurant on load
-    // console.log(setAllRestaurants);
-    setFilteredRestaurants(resData); //show only filtered restaurant search by the user 
-    setLoading(false);
+      // call the checkJsonData() function which return Swiggy Restaurant data
+      const resData = await checkJsonData(json);
+      setAllRestaurants(resData); //initially show all restaurant on load
+      setFilteredRestaurants(resData); //show only filtered restaurant search by the user
+      setLoading(false);
+      setCarouselCards(
+        json?.data?.cards[0]?.card?.card?.gridElements?.infoWithStyle?.info
+      );
+    } catch (error) {
+      console.error("Error fetching restaurant data:", error);
+    }
   }
+
   console.log("render");
 
   //Checking if user is online or offline
-  const isOnline=useOnline();
-  
-  if(!isOnline){
-    return <h1>🔴Oops!! Seems like your offline, Please Check Your Internet🔴</h1>
+  const isOnline = useOnline();
+
+  if (!isOnline) {
+    return (
+      <h1>🔴Oops!! Seems like your offline, Please Check Your Internet🔴</h1>
+    );
   }
-// if not rendered properly this is called: Early return
-if(!allRestaurants) return null;
-// use searchData function and set condition if data is empty show error message
-// if(filteredRestaurants?.length === 0) return <h1>Sorry! No Restaurant Found</h1>
+  // if not rendered properly this is called: Early return
+  if (!allRestaurants) return null;
+  // use searchData function and set condition if data is empty show error message
+  // if(filteredRestaurants?.length === 0) return <h1>Sorry! No Restaurant Found</h1>
 
-//Conditional Rendering
-//if restaurant is empty => shimmer UI
-// else restaurant has data => actual
+  //Conditional Rendering
+  //if restaurant is empty => shimmer UI
+  // else restaurant has data => actual
 
-if (loading) return <Shimmer cards={20}/>;   ;
-return(
+  if (loading) return <Shimmer cards={20} />;
+  return (
     <>
       <div className="search-container my-2 p-2 flex ">
         {/* <marquee> <h1>Stay Disciplined And Stay Focused !! </h1> </marquee> */}
 
-        <input data-testid="search-input" type="text"
+        <input
+          data-testid="search-input"
+          type="text"
           placeholder="Search for restaurants"
-      className="search-input p-2 border-solid border-2 border-black-500 rounded-lg 
+          className="search-input p-2 border-solid border-2 border-black-500 rounded-lg 
        focus:bg-[#FFFBAC]  w-[80%]"
           value={searchText}
           onChange={(e) => {
@@ -81,34 +93,40 @@ return(
         />
         <h3>{searchText}</h3>
         {/* Searching the restaurant */}
-        <button data-testid="search-btn"
-        className="search-button bg-amber-200   py-1 px-2  my-2 rounded-lg text-black ml-2" onClick={() => {
-          const data = filterData(searchText,allRestaurants);
-          setFilteredRestaurants(data);
-        }
-        }>Search</button>
+        <button
+          data-testid="search-btn"
+          className="search-button bg-amber-200   py-1 px-2  my-2 rounded-lg text-black ml-2"
+          onClick={() => {
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
+      <Carousel carouselCards={carouselCards} />
 
-{
-
- <div className="flex flex-wrap md:justify-center" data-testid="res-list">
- {filteredRestaurants.map((eachRestaurant) => {
-   return (
-    <Link className="restaurantMenu-links"
-    to={"/restaurant/" + eachRestaurant?.info?.id}
-    key={eachRestaurant?.info?.id}
-  >
-    <RestaurantCard {...eachRestaurant?.info}  />
-  </Link>
-     );
-   // <RestaurantCard {...restaurant.data} key={restaurant.data.id} />);
- })}
-</div>
-}
-  
-    </> 
+      {
+        <div
+          className="flex flex-wrap md:justify-center"
+          data-testid="res-list"
+        >
+          {filteredRestaurants.map((eachRestaurant) => {
+            return (
+              <Link
+                className="restaurantMenu-links"
+                to={"/restaurant/" + eachRestaurant?.info?.id}
+                key={eachRestaurant?.info?.id}
+              >
+                <RestaurantCard {...eachRestaurant?.info} />
+              </Link>
+            );
+            // <RestaurantCard {...restaurant.data} key={restaurant.data.id} />);
+          })}
+        </div>
+      }
+    </>
   );
 };
 
 export default Body;
-
---
+++ src/components/Carousel.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useRef } from "react";
+import { ITEM_IMG_CDN_URL } from "../constant";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import CarouselBtn from "./CarouselBtn";
+
+const Carousel = ({ carouselCards }) => {
+    console.log("carouselCards:", carouselCards);
+
+    // Check if carouselCards is falsy or not an array
+    if (!carouselCards || !Array.isArray(carouselCards)) return null; 
+  if (!carouselCards) return null; // when api call on small screens remove Carousel
+  const carousel = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [maxIndex, setMaxIndex] = useState(0);
+  const movePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
+  const moveNext = () => {
+    setMaxIndex(
+      Math.floor(carousel.current.scrollWidth / carousel.current.offsetWidth) -
+        1
+    );
+    if (currentIndex <= maxIndex) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (carousel.current !== null) {
+      carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
+    }
+  }, [currentIndex]);
+
+  return (
+    <div className="w-11/12 h-64 flex flex-col mt-10  ">
+      <div className="flex items-center justify-between w-full">
+        <h3 className="font-bold text-xl pl-4 p-5">What's on your mind?</h3>
+        <div className="flex gap-4">
+          <CarouselBtn
+            onClick={movePrev}
+            disabled={currentIndex === 0}
+            icon={faArrowLeft}
+          />
+          <CarouselBtn
+            onClick={moveNext}
+            disabled={currentIndex > maxIndex}
+            icon={faArrowRight}
+          />
+        </div>
+      </div>
+      <div className="w-full h-52 relative overflow-hidden">
+        <div
+          ref={carousel}
+          className="h-full pl-5 flex gap-8 overflow-hidden scroll-smooth"
+        >
+          {carouselCards.map((carouselCard) => (
+            <img
+              key={carouselCard.id}
+              className="object-center h-full w-52 transition-transform hover:scale-110 duration-200 mix-blend-multiply cursor-pointer"
+              src={ITEM_IMG_CDN_URL + carouselCard.imageId}
+              alt="card img"
+            />
+          ))}
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
