c914d9b69059c90e269d52eefd75510946fc7e89
Keegan <keegancolaco20@gmail.com>
2024-02-20 19:41:53 +0530

    Fix: Swiggy APi CORS Error

--- src/constant.js
+++ src/constant.js
@@ -7,13 +7,14 @@ export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/
 export const ITEM_IMG_CDN_URL =
 "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-//swiggy restaurant details with thingproxy
+//swiggy restaurant details with  foodfire server
 export const swiggy_restaurant_details= 
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=15.4934622&lng=73.8327136&page_type=DESKTOP_WEB_LISTING";
+"https://foodfire.onrender.com/api/restaurants/?lat=15.4934622&lng=73.8327136&page_type=DESKTOP_WEB_LISTING";
+
 
 // Swiggy API to get Restaurant Menu data with thingproxy
 export const swiggy_menu_api_URL =
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=15.4909301&lng=73.8278496&&submitAction=ENTER&restaurantId=";
+"https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=15.4909301&lng=73.8278496&&submitAction=ENTER&restaurantId=";
 
 // menu items api card type key
 export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
