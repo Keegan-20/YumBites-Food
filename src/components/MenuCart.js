9af1b98af30ad9f47c0c17862dee96eb21f6a0c9
Keegan <keegancolaco20@gmail.com>
2024-02-27 21:25:05 +0530

    Fix: Added change item quantity feature

--- src/components/MenuCart.js
+++ src/components/MenuCart.js
@@ -1,58 +1,71 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { updateCartItemQuantity } from './utils/cartSlice';
-import { IMG_CDN_URL } from '../constant';
-import 'react-loading-skeleton/dist/skeleton.css';
-import VegNonVeg from './utils/VegNonVeg';
+import { useState,useSelector } from "react";
+import { useDispatch } from "react-redux";
+import { updateCartItemQuantity } from "./utils/cartSlice";
+import { IMG_CDN_URL } from "../constant";
+import "react-loading-skeleton/dist/skeleton.css";
+import VegNonVeg from "./utils/VegNonVeg";
 
 const MenuCart = ({
   name,
-  imageId,price,itemAttribute
+  imageId,
+  price,
+  itemAttribute,
+  
   //   sla // This is the entire 'sla' object
 }) => {
+  // const cartItems=useSelector((store) => store.cart.items);
+
   // managingh the item quantity
-  const [quantity, setQuantity] = useState(1); 
+  const [quantity, setQuantity] = useState(1);
   const dispatch = useDispatch();
 
   const handleQuantityChange = (e) => {
     const newQuantity = parseInt(e.target.value);
     setQuantity(newQuantity);
-    dispatch(updateCartItemQuantity({ itemId: itemAttribute.id, quantity: newQuantity }));
+    dispatch(
+      updateCartItemQuantity({
+        itemId: itemAttribute.id,
+        quantity: newQuantity,
+      })
+    );
   };
   const total = price * quantity;
 
- // Accessing the 'vegClassifier' property from 'itemAttribute'
- const vegClassifierValue = itemAttribute && itemAttribute.vegClassifier;
+  // Accessing the 'vegClassifier' property from 'itemAttribute'
+  const vegClassifierValue = itemAttribute && itemAttribute.vegClassifier;
 
-//  The && (logical AND) operator is used to check if itemAttribute exists (is truthy) before attempting to access the vegClassifier property.
+  //  The && (logical AND) operator is used to check if itemAttribute exists (is truthy) before attempting to access the vegClassifier property.
   //   const { deliveryTime, lastMileTravelString } = sla; // Accessing nested loo
   return (
-    <div className='menuCartContainer flex w-full h-full'>
-    <div className="restaurantItem flex  justify-between items-center w-[70%] m-auto  border border-indigo-100 rounded-lg mb-4 shadow-lg">
-      <img src={IMG_CDN_URL + imageId} className='w-[130px] h-[100px] p-2 rounded-lg bg-fuchsia-300' alt={name} />
-      <div className="card-content m-auto flex justify-between items-center px-2">
-        <div className=' flex '>
-          <h2 className="RestaurantName flex justify-between mr-2 font-medium text-center">{name}</h2>
-          <h4 className='font-semibold mr-5'>
-           <VegNonVeg itemAttribute={itemAttribute} /> 
-          </h4>
-          <h4 className='mr-5 '>{vegClassifierValue} </h4>
-        <h4 className='mr-5 '>₹{total / 100} </h4>
-        <select value={quantity} onChange={handleQuantityChange}>
+    <div className="menuCartContainer flex w-full h-full">
+      <div className="restaurantItem flex  justify-between items-center w-[70%] m-auto  border border-indigo-100 rounded-lg mb-4 shadow-lg">
+        <img
+          src={IMG_CDN_URL + imageId}
+          className="w-[130px] h-[100px] p-2 rounded-lg bg-fuchsia-300"
+          alt={name}
+        />
+        <div className="card-content m-auto flex justify-between items-center px-2">
+          <div className=" flex ">
+            <h2 className="RestaurantName flex justify-between mr-2 font-medium text-center">
+              {name}
+            </h2>
+            <h4 className="font-semibold mr-5">
+              <VegNonVeg itemAttribute={itemAttribute} />
+            </h4>
+            <h4 className="mr-5 ">{vegClassifierValue} </h4>
+            <h4 className="mr-5 ">₹{total / 100} </h4>
+            <select value={quantity} onChange={handleQuantityChange}>
               {[...Array(10).keys()].map((num) => (
-                <option key={num + 1} value={num + 1}>{num + 1}</option>
+                <option key={num + 1} value={num + 1}>
+                  {num + 1}
+                </option>
               ))}
             </select>
+          </div>
         </div>
-
-
       </div>
     </div>
-  </div>
-  
-
   );
-
-}
+};
 
 export default MenuCart;
