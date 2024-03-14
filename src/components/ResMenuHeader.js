6220fe75c4bdfc2e3f9b2a39ee3803aab57b1b76
Keegan <keegancolaco20@gmail.com>
2024-03-14 17:08:59 +0530

    fix:Restaurant Menu Header Responsivness

--- src/components/ResMenuHeader.js
+++ src/components/ResMenuHeader.js
@@ -8,8 +8,7 @@ import {
 import { IMG_CDN_URL } from "../constant.js";
 import { Link } from "react-router-dom";
 
-
-const ResMenuHeader = ({ restaurantOffers,restaurant }) => {
+const ResMenuHeader = ({ restaurantOffers, restaurant }) => {
   const {
     name,
     cuisines,
@@ -22,7 +21,7 @@ const ResMenuHeader = ({ restaurantOffers,restaurant }) => {
     costForTwoMessage,
   } = restaurant;
   console.log(restaurantOffers);
-  
+
   return (
     <>
       <span className="text-[0.8rem] text-gray-500 font-semibold p-2 text-left w-1/3 md:hidden ">
@@ -30,7 +29,7 @@ const ResMenuHeader = ({ restaurantOffers,restaurant }) => {
         <span className="cursor-pointer"> / Central Goa / </span>
         <span className="text-gray-800">{name}</span>
       </span>
-      <div className=" w-full md:w-auto   h-40 md:h-auto rounded-xl p-2 bg-gray-100 flex items-center justify-between border border-green-500">
+      <div className=" w-full md:w-auto   h-40 md:h-auto rounded-xl p-2 bg-gray-100 flex items-center justify-between ">
         <div className="flex flex-col  gap-1 px-4">
           <div className="font-bold text-xl">{name}</div>
           <div className="text-xs text-gray-400 font-medium">
@@ -40,49 +39,47 @@ const ResMenuHeader = ({ restaurantOffers,restaurant }) => {
           <div className="text-xs text-gray-400 font-medium">
             {areaName + ", " + sla?.lastMileTravelString}
           </div>
-          <div className="mt-2 flex items-center semimd:flex-row gap-2 text-gray-500 font-medium text-xs md:text-sm">
+          <div className="mt-2 flex items-center semimd:flex-col  gap-2 text-gray-500 font-medium text-xs md:text-sm">
             {feeDetails?.message && (
-              <>
-                <img
-                  className="h-5 mix-blend-multiply"
-                  src={IMG_CDN_URL + feeDetails?.icon}
-                  alt="icon"
-                />
-                <span>{feeDetails?.message}</span>
-              </>
+              <div className="flex items-center">
+              <img
+                className="h-5 mix-blend-multiply"
+                src={IMG_CDN_URL + feeDetails?.icon}
+                alt="icon"
+              />
+              <span className=" text-base semimd:py-2 semimd:text-xs">{feeDetails?.message}</span>
+            </div>
             )}
             <div className="ml-24 semilg:ml-2">
-              <p className="flex  gap-1 items-center text-sm semilg:text-xs semilg:mb-2">
-            <FontAwesomeIcon icon={faClock} />
-            {sla?.slaString}
-          </p>
-          <p className="flex gap-1 flex-row items-center text-sm semilg:text-xs">
-            <FontAwesomeIcon icon={faRupeeSign} />
-            {costForTwoMessage}
-          </p>
-          </div>
+              <p className="flex gap-1 items-center text-sm semilg:text-xs semilg:mb-2">
+                <FontAwesomeIcon icon={faClock} />
+                {sla?.slaString}
+              </p>
+              <p className="flex gap-1 semimd:flex-row items-center text-sm semilg:text-xs">
+                <FontAwesomeIcon icon={faRupeeSign} />
+                {costForTwoMessage}
+              </p>
+            </div>
           </div>
         </div>
         <div>
-        <img
+          <img
             src={IMG_CDN_URL + cloudinaryImageId}
             className="w-[90px] h-[100px] semimd:w-auto semimd:h-[90px] px-1 py-3"
             alt={name}
           />
-      
 
-        <div className="flex flex-col gap-1 mr-4 rounded-lg border-2 p-1 md:h-14 w-24 text-center bg-gray-50 semimd:w-20">
-          <div className="rounded-lg  text-green-500 text-xs  md:text-sm font-semibold md:font-bold md:border-b-2 border-b pb-1">
-            <FontAwesomeIcon icon={faStar} />
-            {avgRatingString}
+          <div className="flex flex-col gap-1 mr-4 rounded-lg border-2 p-1 md:h-14 w-24 text-center bg-gray-50 semimd:w-20">
+            <div className="rounded-lg  text-green-500 text-xs  md:text-sm font-semibold md:font-bold md:border-b-2 border-b pb-1">
+              <FontAwesomeIcon icon={faStar} />
+              {avgRatingString}
+            </div>
+            <div className=" rounded-lg text-gray-400 text-[0.55rem] md:text-[0.6rem] font-semibold tracking-tighter">
+              {totalRatingsString}
+            </div>
           </div>
-          <div className=" rounded-lg text-gray-400 text-[0.55rem] md:text-[0.6rem] font-semibold tracking-tighter">
-            {totalRatingsString}
-          </div>
-        </div>
         </div>
       </div>
-    
     </>
   );
 };
